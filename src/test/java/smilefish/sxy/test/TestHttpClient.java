acd7a7b0bc30797866fcd11e862e509e7b8f6f79
hanliang6 <hanliang1@jd.com>
2015-12-04 12:09:39 +0800

    created tested httputil

---
+++ src/test/java/smilefish/sxy/test/TestHttpClient.java
@@ -0,0 +1,35 @@
+package smilefish.sxy.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import smilfish.xyz.util.HttpUtil;
+import smilfish.xyz.util.impl.HttpUtilImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hanliang1 on 2015/12/4.
+ */
+public class TestHttpClient {
+
+    private HttpUtil httpUtil = new HttpUtilImpl();
+
+    @Test
+    public void testGet() throws Exception {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("keyword", "kindle");
+        String result = httpUtil.get("http://search.jd.com/Search", params);
+        Assert.assertNotNull(result);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testPost() throws Exception {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("log", "asd");
+        params.put("pwd", "asd");
+        String result = httpUtil.post("http://blog.com/wp-login.php", params);
+        System.out.println(result);
+    }
+}
